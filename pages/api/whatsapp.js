b573f32917d5888428748e5cd6805a7eab69553a
Murilo Cruz de Santana <murilo.cruzsantana@gmail.com>
2023-05-26 11:48:52 -0300

    zapzap

--- pages/api/whatsapp.js
+++ pages/api/whatsapp.js
@@ -1,7 +1,11 @@
+import dynamic from "next/dynamic";
+
 function whatsapp(request,response){
+    const dynamicDate = new Date();
+
     response.json({
-        cod: request
-    })
+        date: dynamicDate.ToGMTString()
+    });
 }
 
 export default whatsapp;
