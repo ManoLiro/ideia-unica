5d2dfeb16e52eb2d2f80a8f7665985cdc39dbc5c
Murilo Cruz de Santana <murilo.cruzsantana@gmail.com>
2023-05-26 12:09:29 -0300

    zapzap

--- pages/api/whatsapp.js
+++ pages/api/whatsapp.js
@@ -1,22 +1,47 @@
-import dynamic from "next/dynamic";
-const http = require('http');
-const url = require('url');
+import { useEffect } from 'react';
+import { Server } from 'ws';
 
-function whatsapp(request,response){
-    const queryData = url.parse(request.url, true).query;
-  
-    // Acessar os parâmetros da solicitação GET
-    const parameter1 = queryData.parameter1;
-    const parameter2 = queryData.parameter2;
-  
-    // Realizar ações com base nos parâmetros recebidos
-    // Aqui você pode processar os parâmetros da solicitação e fazer o que for necessário
-  
-    // Responder à solicitação
-    response.statusCode = 200;
-    response.setHeader('Content-Type', 'text/plain');
-    response.end('Webhook GET recebido com sucesso.');
-  }
+export default function WhatsappWebhook() {
+  useEffect(() => {
+    const socket = new WebSocket('https://ideia-unica-nu-nine.vercel.app/api/whatsapp');
 
+    socket.onopen = () => {
+      console.log('Conexão estabelecida com o servidor WebSocket.');
+    };
 
-export default whatsapp;
+    socket.onmessage = event => {
+      const message = JSON.parse(event.data);
+      console.log('Nova mensagem recebida:', message);
+      // Faça o que for necessário com a mensagem em tempo real
+    };
+
+    socket.onclose = event => {
+      console.log('Conexão WebSocket fechada. Código:', event.code);
+    };
+
+    return () => {
+      socket.close();
+    };
+  }, []);
+
+  return <div>Webhook em tempo real com Next.js e WebSocket para a rota /api/whatsapp</div>;
+}
+
+export async function getServerSideProps({ req, res }) {
+  const wss = new Server({ noServer: true });
+
+  wss.on('connection', socket => {
+    socket.on('message', message => {
+      // Aqui você pode processar a mensagem recebida e enviar para os clientes conectados em tempo real
+      wss.clients.forEach(client => {
+        if (client.readyState === WebSocket.OPEN) {
+          client.send(JSON.stringify(message));
+        }
+      });
+    });
+  });
+
+  res.socket.server.wss = wss;
+
+  return { props: {} };
+}
